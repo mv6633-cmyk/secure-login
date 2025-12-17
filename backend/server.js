f6c44f3989533db1262c4fd09438eb4a720cd5b3
maryshivani <maryshivani.24it@licet.ac.in>
2025-12-17 20:48:41 +0530

    Deploy edit

--- backend/server.js
+++ backend/server.js
@@ -59,6 +59,6 @@ app.post("/login", (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
-  console.log("✅ Server running on http://localhost:5000");
+app.listen(3000, () => {
+  console.log("✅ Server running on http://localhost:3000");
 });
